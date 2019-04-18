069cfe5589f68599d8b900cfd6cfa3779f20876e
Zep Fietje <44533235+zepfietje@users.noreply.github.com>
2019-04-18 23:33:43 +0200

    Make screen sizes util more readable

--- lib/utils/screen_sizes.dart
+++ lib/utils/screen_sizes.dart
@@ -1,28 +1,32 @@
 import 'package:flutter/material.dart';
 
-/// Divide screen sizes into 3 categories
+/// Divides screen sizes into three categories.
 enum ScreenSizes {
   small,
   medium,
   large,
 }
 
-/// Get the size of the screen
+/// Gets the screen size category of the screen.
 ScreenSizes screenSize(BuildContext context) {
   final double width = MediaQuery.of(context).size.width;
-  if (width < 340) return ScreenSizes.small;
-  if (width < 540) return ScreenSizes.medium;
-  return ScreenSizes.large;
+  if (width < 340) {
+    return ScreenSizes.small;
+  } else if (width < 540) {
+    return ScreenSizes.medium;
+  } else {
+    return ScreenSizes.large;
+  }
 }
 
-/// Vertical space that 1 year uses
+/// Gets the vertical space that one year takes up.
 double getYearViewHeight(context) {
   final int verticalDays = 6;
   final int verticalMonths = 4;
   return verticalDays * verticalMonths * getDayNumberSize(context) + 330.0;
 }
 
-/// Get the space that the container of 1 day number uses
+/// Gets the space that the container of one day number takes up.
 double getDayNumberSize(context) {
   return (MediaQuery.of(context).size.width - 72) / 21;
 }
