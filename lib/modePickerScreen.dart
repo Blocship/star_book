19c4b440d01570b71e6701d7718ef17961ed0e65
hashirshoaeb <hashirshoaeb@gmail.com>
2020-06-14 19:36:59 +0500

    code readablility improvment of mood picker

--- lib/modePickerScreen.dart
+++ lib/modePickerScreen.dart
@@ -1,82 +1,94 @@
 import 'package:flutter/material.dart';
 
-class _ModePickerState extends State<ModePicker> {
-
-  String label='';
-  double _value=1.0;
-  List<String> modes=['Routine','Happy','sad','angry','normal'];
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primaryColor: Colors.blue[100],
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: Scaffold(
-        appBar: AppBar(
-                        title:Text('Mode Indicator!',style: TextStyle(color: Colors.white,fontSize: 23,fontWeight: FontWeight.bold),),
-                        leading: Icon(Icons.child_care,color: Colors.white,size:60 ,),
-                        centerTitle: true,
-
-                        primary: true,
-                        ),
-        body: Container(
-          alignment: Alignment.center,
-          padding: EdgeInsets.all(30),
-          color: Colors.blue[100],
-          child:  Column(
-            children: <Widget>[
-              Container(
-                color: Colors.transparent,
-                height: 100,
-              ),
-             Container(
-                color: Colors.transparent,
-                height: 30,
-                child: Text('How was your day today?',style: TextStyle(fontSize: 25,color: Colors.white)),
-              ),
-              Container(
-                color: Colors.transparent,
-                height: 20,
-              ),
-              Container(
-                color: Colors.transparent,
-                height: 125,
-                child:  (_value ==1)?Icon(Icons.child_care,size: 130,color: Colors.red[200],):(_value ==2)?Icon(Icons.child_care,size: 130,color: Colors.green[200],):(_value ==3)?Icon(Icons.child_care,size: 130,color: Colors.green[500],):(_value ==4)?Icon(Icons.child_care,size: 130,color: Colors.orange[300],):Icon(Icons.child_care,size: 130,color: Colors.white,),
-              ),
-              Container(
-                color: Colors.transparent,
-                height: 20,
-              ),
-              Container(
-                color: Colors.transparent,
-                height: 100,
-                child:Slider(
-                   min: 0,
-                   max: 4,
-                   value: _value,
-                  onChanged: (value) {
-                   setState(() {
-                      _value = value;
-                         });
-                            },
-                            divisions: 4,
-                            label: (_value ==1)?"sad":(_value ==2)?"average":(_value ==3)?"happy":(_value ==4)?"productive":null,
-                     ),
-              ),
-            ],
-          )
-        ),
-      ),
-    );
-  }
-}
-
 class ModePicker extends StatefulWidget {
   ModePicker({Key key}) : super(key: key);
 
   @override
   State<StatefulWidget> createState() => _ModePickerState();
+}
+
+class _ModePickerState extends State<ModePicker> {
+  String label = '';
+  double _value = 1.0;
+  List<String> modes = ['Routine', 'Happy', 'sad', 'angry', 'normal'];
+
+  Widget _getIcon(BuildContext context) {
+    if (_value == 1)
+      return Icon(Icons.child_care, size: 130, color: Colors.red[200]);
+    else if (_value == 2)
+      return Icon(Icons.child_care, size: 130, color: Colors.green[200]);
+    else if (_value == 3)
+      return Icon(Icons.child_care, size: 130, color: Colors.green[500]);
+    else if (_value == 4)
+      return Icon(Icons.child_care, size: 130, color: Colors.orange[300]);
+    else
+      return Icon(Icons.child_care, size: 130, color: Colors.white);
+  }
+
+  String _getLabel(BuildContext context) {
+    if (_value == 1)
+      return "sad";
+    else if (_value == 2)
+      return "average";
+    else if (_value == 3)
+      return "happy";
+    else if (_value == 4)
+      return "productive";
+    else
+      return null;
+  }
+
+  void _onSliderChanged(value) {
+    setState(() {
+      _value = value;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.center,
+      padding: EdgeInsets.all(30),
+      color: Colors.blue[100],
+      child: Column(
+        children: <Widget>[
+          Container(
+            color: Colors.transparent,
+            height: 100,
+          ),
+          Container(
+            color: Colors.transparent,
+            height: 30,
+            child: Text('How was your day today?',
+                style: TextStyle(fontSize: 25, color: Colors.white)),
+          ),
+          Container(
+            color: Colors.transparent,
+            height: 20,
+          ),
+          Container(
+            color: Colors.transparent,
+            height: 125,
+            child: _getIcon(context),
+          ),
+          Container(
+            color: Colors.transparent,
+            height: 20,
+          ),
+          Container(
+            color: Colors.transparent,
+            height: 100,
+            child: Slider(
+              min: 0,
+              max: 4,
+              value: _value,
+              onChanged: _onSliderChanged,
+              divisions: 4,
+              label: _getLabel(context),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 }
