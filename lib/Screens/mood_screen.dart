58c9004f7b44601f55b1c808f8e89af5f91c7e25
Shahzad Umar Baig <shahzadubaig@gmail.com>
2020-07-25 11:21:23 +0500

    Created mood screen UI and added working functionality

--- lib/Screens/mood_screen.dart
+++ lib/Screens/mood_screen.dart
@@ -9,15 +9,62 @@ class _MoodScreenState extends State<MoodScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        leading: IconButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          icon: Icon(Icons.arrow_back),
+      body: Container(
+        height: double.infinity,
+        width: double.infinity,
+        child: Stack(
+          fit: StackFit.loose,
+          children: [
+            Container(
+              padding: EdgeInsets.symmetric(vertical: 50, horizontal: 30),
+              child: Hero(
+                tag: 'UserIcon',
+                child: CircleAvatar(
+                  radius: 40,
+                  backgroundImage: AssetImage('images/user.png'),
+                ),
+              ),
+            ),
+            Positioned(
+              child: Align(
+                alignment: Alignment.topLeft,
+                child: Text(
+                  "How was your Day Today?",
+                  style: TextStyle(fontSize: 23),
+                ),
+              ),
+              top: 160,
+              left: 20,
+            ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Container(
+                padding: EdgeInsets.symmetric(
+                  vertical: 10,
+                  horizontal: 10,
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    IconButton(
+                      icon: Icon(Icons.arrow_back_ios),
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      color: Colors.blueAccent,
+                    ),
+                    IconButton(
+                      icon: Icon(Icons.arrow_forward_ios),
+                      onPressed: () {},
+                      color: Colors.blueAccent,
+                    )
+                  ],
+                ),
+              ),
+            )
+          ],
         ),
       ),
-      body: Container(),
     );
   }
 }
