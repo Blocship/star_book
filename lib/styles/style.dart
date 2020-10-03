c17e3c3561ed5a69f13810d93b7e358aecb7ddbc
hashirshoaeb <hashirshoaeb@gmail.com>
2020-10-03 10:49:54 +0500

    common style sheet added

---
+++ lib/styles/style.dart
@@ -0,0 +1,9 @@
+abstract class Style {
+  // static fileds for common styling
+  // static const TextStyle productRowItemName = TextStyle(
+  //   color: Color.fromRGBO(0, 0, 0, 0.8),
+  //   fontSize: 18,
+  //   fontStyle: FontStyle.normal,
+  //   fontWeight: FontWeight.normal,
+  // );
+}
