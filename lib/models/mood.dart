a0566fab70b8cb35b844db533b9460f2eede4006
hashirshoaeb <hashirshoaeb@gmail.com>
2020-10-04 14:40:30 +0500

    model updated

--- lib/models/mood.dart
+++ lib/models/mood.dart
@@ -1,3 +1,5 @@
+import '../utils/color.dart';
+
 class Mood {
   Mood({
     this.id,
@@ -10,9 +12,9 @@ class Mood {
 }
 
 List<Mood> mMoodList = new List<Mood>()
-  ..add(Mood(id: 0, label: "Happy", colorCode: 0xff00b9ff))
-  ..add(Mood(id: 1, label: "Sad", colorCode: 0xffe000ff))
-  ..add(Mood(id: 2, label: "Productive", colorCode: 0xff00ffa2))
-  ..add(Mood(id: 3, label: "Sick", colorCode: 0xfffeff00))
-  ..add(Mood(id: 4, label: "Normal", colorCode: 0xffffc600))
-  ..add(Mood(id: 5, label: "Angry", colorCode: 0xffff6100));
+  ..add(Mood(id: 0, label: "Happy", colorCode: Color.systemBlue.index))
+  ..add(Mood(id: 1, label: "Sad", colorCode: Color.systemIndigo.index))
+  ..add(Mood(id: 2, label: "Productive", colorCode: Color.systemGreen.index))
+  ..add(Mood(id: 3, label: "Sick", colorCode: Color.systemYellow.index))
+  ..add(Mood(id: 4, label: "Normal", colorCode: Color.systemOrange.index))
+  ..add(Mood(id: 5, label: "Angry", colorCode: Color.systemRed.index));
