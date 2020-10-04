625df5efb6978baac012d7e16cebc23656e7c0f6
hashirshoaeb <hashirshoaeb@gmail.com>
2020-10-04 14:42:40 +0500

    mood sheet added

---
+++ lib/screens/mood_sheet.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/cupertino.dart' as c;
+import 'package:flutter/widgets.dart';
+// Files
+import '../utils/color.dart';
+import '../widgets/color_container.dart';
+import '../models/mood.dart';
+
+class MoodSheet extends StatelessWidget {
+  c.CupertinoNavigationBar _buildNavBar() {
+    return c.CupertinoNavigationBar(
+      middle: Text("Mood"),
+      leading: Container(
+          padding: EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+          child: Text("back")),
+      trailing: null,
+      border: null,
+    );
+  }
+
+  c.SafeArea _buildBody(BuildContext context) {
+    return SafeArea(
+      // minimum: EdgeInsets.symmetric(horizontal: 16),
+      child: c.Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Padding(padding: EdgeInsets.symmetric(vertical: 18)),
+          ...mMoodList.map((e) {
+            return ColorContainer(
+              text: e.label,
+              color: getColor(Color.values[e.colorCode]),
+              // TODO: add keys
+              // On Tap functionality
+            );
+          }),
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return c.CupertinoPageScaffold(
+      backgroundColor: c.CupertinoColors.systemGroupedBackground,
+      navigationBar: _buildNavBar(),
+      child: _buildBody(context),
+    );
+  }
+}
