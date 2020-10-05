833bfb1b064c6b5a8f6a042f622926613c588871
hashirshoaeb <hashirshoaeb@gmail.com>
2020-10-05 18:01:54 +0500

    route generators added

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/cupertino.dart' as c;
 import 'package:flutter/widgets.dart';
 // Files
-import 'screens/home_page.dart';
+import 'routes/route_generator.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,9 +11,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return c.CupertinoApp(
-      home: HomePage(),
+      initialRoute: '/',
       title: "StarBook",
       theme: c.CupertinoThemeData(),
+      onGenerateRoute: (settings) => RouteGenerator.mainRoute(settings),
     );
   }
 }
---
+++ lib/routes/route_generator.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/widgets.dart';
+import "package:flutter/cupertino.dart" as c;
+// Files
+import '../screens/activity_edit_sheet.dart';
+import '../screens/activity_page.dart';
+import '../screens/error_page.dart';
+import '../screens/home_page.dart';
+import '../screens/mood_sheet.dart';
+
+class RouteGenerator {
+  static Route<dynamic> mainRoute(RouteSettings settings) {
+    final args = settings.arguments;
+    switch (settings.name) {
+      case "/":
+        return c.CupertinoPageRoute(builder: (context) => HomePage());
+      case "/activity":
+        return c.CupertinoPageRoute(builder: (context) => ActivityPage());
+      default:
+        return c.CupertinoPageRoute(builder: (context) => ErrorPage());
+    }
+  }
+
+  static Route<dynamic> activityRoute(RouteSettings settings) {
+    final args = settings.arguments;
+    switch (settings.name) {
+      case "/edit":
+        return c.CupertinoPageRoute(builder: (context) => ActivityEditSheet());
+      case "/mood":
+        return c.CupertinoPageRoute(builder: (context) => MoodSheet());
+      default:
+        return c.CupertinoPageRoute(builder: (context) => ErrorPage());
+    }
+  }
+}
