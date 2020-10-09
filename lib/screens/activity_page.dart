2c88eb753c4258a8f563755a2258b2a9050a956e
ShahzadUmarBaig <ShahzadUBaig@gmail.com>
2020-10-09 08:25:23 -0700

    Hardcoded the activity file

--- lib/screens/activity_page.dart
+++ lib/screens/activity_page.dart
@@ -12,10 +12,43 @@ class ActivityPage extends StatelessWidget {
         middle: Text('Activity'),
       ),
       child: SafeArea(
-          bottom: false,
-          child: Center(
-            child: Text("Show Activity"),
-          )),
+        bottom: false,
+        child: c.Column(
+          crossAxisAlignment: c.CrossAxisAlignment.start,
+          children: [
+            c.Container(
+              padding: c.EdgeInsets.all(16),
+              child: c.Row(
+                mainAxisAlignment: c.MainAxisAlignment.center,
+                children: [
+                  c.Text(
+                    "07 - 10 - 20",
+                    style: c.TextStyle(
+                        fontWeight: c.FontWeight.bold, fontSize: 24),
+                  ),
+                ],
+              ),
+            ),
+            c.Container(
+              padding: c.EdgeInsets.all(16),
+              child: c.Text("Happy"),
+            ),
+            c.Container(
+              padding: c.EdgeInsets.all(16),
+              child: c.Text("Title"),
+            ),
+            c.Container(
+              padding: c.EdgeInsets.all(16),
+              child: c.Text("Note: \n\nLorem Ipsum is simply dummy text of the printing and typesetting industry." +
+                  " Lorem Ipsum has been the industry's standard dummy text ever since the 1500s," +
+                  " when an unknown printer took a galley of type and scrambled it to make a type specimen book." +
+                  " It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. " +
+                  " It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages," +
+                  " and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
