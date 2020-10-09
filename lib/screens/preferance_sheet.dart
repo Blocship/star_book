93d9fa6cfad671172f9c15ac6d4365b8bec06a48
ShahzadUmarBaig <shahzadubaig@gmail.com>
2020-10-09 01:23:22 -0700

    Merge branch 'master' of https://github.com/hashirshoaeb/star_book into master
    # Conflicts:
    #	.vscode/settings.json
    #	android/app/src/main/AndroidManifest.xml
    #	lib/models/activity.dart
    #	lib/models/mood.dart

--- lib/screens/preferance_sheet.dart
+++ lib/screens/preferance_sheet.dart
@@ -40,14 +40,8 @@ class PreferanceSheet extends StatelessWidget {
               c.Container(
                 padding: c.EdgeInsets.all(0),
                 margin: c.EdgeInsets.all(0),
-                width: c.MediaQuery
-                    .of(context)
-                    .size
-                    .width / 4,
-                height: c.MediaQuery
-                    .of(context)
-                    .size
-                    .height / 8,
+                width: c.MediaQuery.of(context).size.width / 4,
+                height: c.MediaQuery.of(context).size.height / 8,
                 decoration: c.BoxDecoration(
                   color: c.CupertinoColors.black,
                   shape: c.BoxShape.circle,
