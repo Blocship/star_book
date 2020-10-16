012604777d83f7f9000fbe2d3095933f9ebb9a32
hashirshoaeb <hashirshoaeb@gmail.com>
2020-10-16 20:51:09 +0500

    routing paths updated

--- lib/routes/route_generator.dart
+++ lib/routes/route_generator.dart
@@ -6,31 +6,57 @@ import '../screens/activity_page.dart';
 import '../screens/error_page.dart';
 import '../screens/home_page.dart';
 import '../screens/mood_sheet.dart';
+import '../screens/preferance_sheet.dart';
+import '../utils/bottom_sheet.dart';
 
 class RouteGenerator {
-  static Route<dynamic> mainRoute(RouteSettings settings) {
+  static Route<dynamic> mainRoute(
+    RouteSettings settings,
+  ) {
     final args = settings.arguments;
     switch (settings.name) {
       case "/":
-        return MaterialWithModalsPageRoute(builder: (context) => HomePage());
+        return MaterialWithModalsPageRoute(
+          builder: (context) => HomePage(),
+        );
       case "/activity":
         return MaterialWithModalsPageRoute(
-            builder: (context) => ActivityPage(args));
+          builder: (context) => ActivityPage(args),
+        );
+      case "/preferance":
+        return ModalBottomSheetRouteWrapper(
+          builder: (context, controller) => PreferanceSheet(),
+        );
+      case "/edit":
+        return ModalBottomSheetRouteWrapper(
+          builder: (context, controller) =>
+              ActivityEditSheetRouteInitializer(args),
+        );
       default:
-        return MaterialWithModalsPageRoute(builder: (context) => ErrorPage());
+        return MaterialWithModalsPageRoute(
+          builder: (context) => ErrorPage(),
+        );
     }
   }
 
-  static Route<dynamic> activityRoute(RouteSettings settings) {
+  static Route<dynamic> activityRoute(
+    RouteSettings settings,
+    dynamic activity,
+  ) {
     final args = settings.arguments;
     switch (settings.name) {
       case "/edit":
         return MaterialWithModalsPageRoute(
-            builder: (context) => ActivityEditSheet());
+          builder: (context) => ActivityEditSheet(activity),
+        );
       case "/mood":
-        return MaterialWithModalsPageRoute(builder: (context) => MoodSheet());
+        return MaterialWithModalsPageRoute(
+          builder: (context) => MoodSheet(),
+        );
       default:
-        return MaterialWithModalsPageRoute(builder: (context) => ErrorPage());
+        return MaterialWithModalsPageRoute(
+          builder: (context) => ErrorPage(),
+        );
     }
   }
 }
