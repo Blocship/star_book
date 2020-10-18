0c12c8aed49e3ee48b8678cca823d5bba4d02231
hashirshoaeb <hashirshoaeb@gmail.com>
2020-10-18 21:35:34 +0500

    model change breaking fixings

--- lib/screens/activity_page.dart
+++ lib/screens/activity_page.dart
@@ -2,13 +2,17 @@ import 'package:flutter/cupertino.dart' as c;
 import 'package:flutter/widgets.dart';
 // Files
 import '../models/activity.dart';
+import '../models/mood.dart';
 
 // TODO: display activity in cupertino style,
 // added just to implement route,
 // will complete it later.
 class ActivityPage extends StatelessWidget {
-  ActivityPage(this.activity);
+  // TODO: moodID and color id is same, this may break change later,
+  // will get the mood from list on the basis of moodId.
+  ActivityPage(this.activity) : mood = mMoodList[activity.moodId];
 
+  final Mood mood;
   final Activity activity;
 
   @override
@@ -44,7 +48,7 @@ class ActivityPage extends StatelessWidget {
               ),
               c.Container(
                 padding: c.EdgeInsets.all(16),
-                child: c.Text(activity.mood.label),
+                child: c.Text(mood.label),
               ),
               c.Container(
                 padding: c.EdgeInsets.symmetric(vertical: 8, horizontal: 16),
