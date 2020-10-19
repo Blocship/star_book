ae6531fdea65a9b587d40cb7d2a562f0657b652b
hashirshoaeb <hashirshoaeb@gmail.com>
2020-10-20 02:26:30 +0500

    some mock data for testing added

--- lib/models/activity.dart
+++ lib/models/activity.dart
@@ -110,4 +110,37 @@ List<Activity> mActivityList = new List<Activity>()
         title: "Ninth Day of Oct",
         note:
             "Aenean diam mi, euismod sed euismod at, in enim. Quisque in suscipit nunc. Mauris neque lorem, sit amet placerat risus viverra pulvinar. In in libero mi. Donec ex efficitur suscipit ac placerat lorem. Nullam magna massa, auctor in dictum ac, elementum nec diam. Maecenas in suscipit tellus."),
+  )
+  ..add(
+    Activity(
+        id: 4,
+        day: 7,
+        month: 9,
+        year: 2020,
+        moodId: 2,
+        title: "Seventh Day of Oct",
+        note:
+            "Aenean diam mi, euismod sed euismod at, condimentum in enim. Quisque in suscipit nunc. Mauris faucibus neque lorem, sit amet placerat risus viverra pulvinar. In in libero mi. Donec ornare turpis sed. Nullam magna massa, auctor in dictum ac, elementum nec diam. Maecenas in suscipit tellus."),
+  )
+  ..add(
+    Activity(
+        id: 5,
+        day: 8,
+        month: 9,
+        year: 2020,
+        moodId: 3,
+        title: "Eighth Day of Oct",
+        note:
+            "Aenean diam mi, euismod sed euismod at, condimentum in enim. Quisque in suscipit nunc. Mauris faucibus neque lorem, sit amet placerat risus viverra pulvinar. In in libero mi. Donec ornare turpis sed ex efficitur suscipit ac placerat lorem."),
+  )
+  ..add(
+    Activity(
+        id: 6,
+        day: 9,
+        month: 9,
+        year: 2020,
+        moodId: 4,
+        title: "Ninth Day of Oct",
+        note:
+            "Aenean diam mi, euismod sed euismod at, in enim. Quisque in suscipit nunc. Mauris neque lorem, sit amet placerat risus viverra pulvinar. In in libero mi. Donec ex efficitur suscipit ac placerat lorem. Nullam magna massa, auctor in dictum ac, elementum nec diam. Maecenas in suscipit tellus."),
   );
