80a7038287f6a6a96eb181aad638bcd8ab945fe1
khyatisaini <sainikhyati2001@gmail.com>
2021-01-08 00:22:12 +0530

    CupertinoSlidingSegmentControl added

--- lib/screens/preferance_sheet.dart
+++ lib/screens/preferance_sheet.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/cupertino.dart' as c;
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:star_book/widgets/custom_switch.dart';
 import 'package:url_launcher/url_launcher.dart';
 // Files
 import '../widgets/action_container.dart';
@@ -10,7 +9,15 @@ import '../styles/style.dart';
 
 /// Preferance Sheet Screen widget displays the
 /// settings option, Privacy Policy, LICENCE, Terms and Conditions etc.
-class PreferanceSheet extends StatelessWidget {
+class PreferanceSheet extends StatefulWidget {
+  // theme mode
+  String _selectedMode = 'Dark';
+
+  @override
+  PreferenceSheetState createState() => PreferenceSheetState();
+}
+
+class PreferenceSheetState extends State<PreferanceSheet> {
   c.CupertinoNavigationBar _buildNavBar(BuildContext context) {
     return c.CupertinoNavigationBar(
       backgroundColor: c.CupertinoDynamicColor.resolve(
@@ -50,32 +57,31 @@ class PreferanceSheet extends StatelessWidget {
       ),
       MyContainer(
           child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Container(
-            width: _textWidth,
-            child: Text(
-              "I'm Hashir, the developer of this app. Feel free to contact me anytime. I love hearing from you",
-              style: Style.body(context),
-            ),
-          ),
-          Container(
-            width: _imageWidth,
-            height: _imageWidth,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(_imageWidth / 2),
-              image: DecorationImage(
-                image: AssetImage("dev-profile.jpeg"),
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                width: _textWidth,
+                child: Text(
+                  "I'm Hashir, the developer of this app. Feel free to contact me anytime. I love hearing from you",
+                  style: Style.body(context),
+                ),
               ),
-            ),
-          ),
-        ],
-      ))
+              Container(
+                width: _imageWidth,
+                height: _imageWidth,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(_imageWidth / 2),
+                  image: DecorationImage(
+                    image: AssetImage("dev-profile.jpeg"),
+                  ),
+                ),
+              ),
+            ],
+          ))
     ];
   }
 
   Widget _buildBody(BuildContext context) {
-    bool _light = false;
     return SafeArea(
       // minimum: EdgeInsets.symmetric(horizontal: 16),
       child: Column(
@@ -88,28 +94,90 @@ class PreferanceSheet extends StatelessWidget {
           // ),
           Padding(padding: EdgeInsets.symmetric(vertical: 18)),
           MyContainer(
-              child: Row(
-                mainAxisAlignment: c.MainAxisAlignment.spaceBetween,
-                children: [
-                  c.Container(
+            child: Row(
+              mainAxisAlignment: c.MainAxisAlignment.start,
+              children: [
+                c.Expanded(
+                  flex: 1,
+                  child: CircleAvatar(
+                    backgroundColor: Colors.white10,
+                    child: Icon(Icons.bedtime, color: Colors.white70, size: 20),
+                  ),
+                ),
+                c.SizedBox(width: 10),
+                c.Expanded(
+                  flex: 3,
+                  child: c.Container(
                     width: MediaQuery.of(context).size.width * 0.6 - 29,
-                    child: Text('Dark Mode',
+                    child: Text('Dark Mode'.toUpperCase(),
                         style: Style.body(context)
                     ),
                   ),
-                  Card(
-                    child: CustomSwitch(
-                        activeColor: Colors.blue,
-                        value: _light,
-                        onChanged: (value) {
-                          _light = value;
-                        }),
-                    shape: c.RoundedRectangleBorder(
-                      borderRadius: c.BorderRadius.circular(18),
-                    ),
+                ),
+                c.Expanded(
+                  flex: 4,
+                  child: c.CupertinoSlidingSegmentedControl(
+                    children: {
+                      'Auto': Container(
+                        width: 40,
+                        height: 40,
+                        child: Card(
+                          elevation: 0,
+                          color: widget._selectedMode == 'Auto' ? Colors.grey : Colors.transparent,
+                          shape: c.RoundedRectangleBorder(
+                            borderRadius: c.BorderRadius.circular(8),
+                          ),
+                          // margin: EdgeInsets.all(10),
+                          child: Container(
+                            width: 25,
+                            height: 25,
+                            decoration: c.BoxDecoration(
+                              color: Colors.white70,
+                              borderRadius: c.BorderRadius.circular(20),
+                            ),
+                            margin: EdgeInsets.all(5),
+                            alignment: c.Alignment.center,
+                            child: Text('A', style: c.TextStyle(color: Colors.black45, fontSize: 15, fontWeight: c.FontWeight.w600)),
+                          ),
+                        ),
+                      ),
+                      'Light': Container(
+                        width: 40,
+                        height: 40,
+                        child: Card(
+                          elevation: 0,
+                          color: widget._selectedMode == 'Light' ? Colors.grey : Colors.transparent,
+                          shape: c.RoundedRectangleBorder(
+                            borderRadius: c.BorderRadius.circular(8),
+                          ),
+                          // margin: EdgeInsets.all(10),
+                          child: Icon(Icons.wb_sunny, color: Colors.white70, size: 20),
+                        ),
+                      ),
+                      'Dark': Container(
+                        width: 40,
+                        height: 40,
+                        child: Card(
+                          elevation: 0,
+                          color: widget._selectedMode == 'Dark' ? Colors.grey : Colors.transparent,
+                          shape: c.RoundedRectangleBorder(
+                            borderRadius: c.BorderRadius.circular(8),
+                          ),
+                          // margin: EdgeInsets.all(10),
+                          child: Icon(Icons.bedtime, color: Colors.white70, size: 20),
+                        ),
+                      ),
+                    },
+                    onValueChanged: (value) {
+                      setState(() {
+                        widget._selectedMode = value;
+                        print(widget._selectedMode);
+                      });
+                    },
                   ),
-                ],
-              ),
+                )
+              ],
+            ),
           ),
           Padding(padding: EdgeInsets.symmetric(vertical: 18)),
           ..._aboutDeveloper(context),
