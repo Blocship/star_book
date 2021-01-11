9b974b3cec4a5c37d3791e0021c15b12b5e8379c
hashirshoaeb <hashirshoaeb@gmail.com>
2021-01-12 01:57:52 +0500

    api refactoring

--- lib/APIService/APIService.dart
+++ lib/APIService/APIService.dart
@@ -1,15 +1,46 @@
 import 'dart:convert';
-import 'package:flutter/cupertino.dart';
+
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:http/http.dart' as http;
+import 'package:http/http.dart';
 
-class APIService {
+// Files
+import '../models/unsplash_photo.dart';
 
-  Future<List> getImages() async {
-    debugPrint(DotEnv().env['API_URL']);
-    var onlineData = await http.get(DotEnv().env['API_URL']);
-    var jsonData = json.decode(onlineData.body);
-    return jsonData['results'];
+const String _api = 'https://api.unsplash.com/';
+
+class UnsplashAPIService {
+  static void loadenv() async {
+    try {
+      await DotEnv().load('.env');
+    } catch (e) {
+      print('.env loading: $e');
+    }
   }
 
+  static Future<List<UnsplashPhoto>> getPhotos(int count) async {
+    assert(count > 0 && count <= 30);
+
+    List<UnsplashPhoto> photos = List<UnsplashPhoto>();
+    const String _path = 'photos/random/';
+    final String _clientID = DotEnv().env['CLIENT_ID'];
+    final String query =
+        '?count=$count&client_id=$_clientID&query=nature&orientation=portrait';
+    try {
+      print('$_api$_path$query');
+      Response response = await get('$_api$_path$query');
+      if (response.statusCode == 200) {
+        List<dynamic> photosdata = json.decode(response.body);
+        photosdata.forEach((photoData) {
+          UnsplashPhoto photo = UnsplashPhoto.fromMap(photoData);
+          photos.add(photo);
+        });
+      } else {
+        print('Unsplash status Code: ${response.statusCode}');
+      }
+      return photos;
+    } catch (e) {
+      print('Unsplash Error: $e');
+      return photos;
+    }
+  }
 }
