0f273dad1a2d6082395a01049e473eed2f49532c
hashirshoaeb <hashirshoaeb@gmail.com>
2021-01-16 16:44:43 +0500

    global setting mock controller added

--- lib/controllers/global_setting.dart
+++ lib/controllers/global_setting.dart
@@ -1,1 +1,24 @@
-class GlobalSettingController {}
+import 'package:hive/hive.dart';
+
+// Files
+import '../models/global_setting.dart';
+
+class GlobalSettingController {
+  User getuser() {
+    return User(name: 'John Deo');
+  }
+
+  void setUser(User user) {}
+
+  BrightnessOption getBrightnessOption() {
+    return BrightnessOption.auto;
+  }
+
+  void setbrightnessOption(BrightnessOption brightnessOption) {}
+
+  DateTime getReminderTime() {
+    return DateTime.now();
+  }
+
+  void setReminderTime(DateTime reminderTime) {}
+}
