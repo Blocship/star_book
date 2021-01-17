54ba8a10d6091f569f9b028dd5b01bad5045c2c2
Adarsh Srivastava <imadarshsri@gmail.com>
2021-01-18 00:37:27 +0530

    Adds FadeInImage for Unsplash Background images
    This adds a FadeInImage Widget to display the Unsplash Images as App Background Images

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,11 +1,14 @@
-import 'package:flutter/cupertino.dart' as c;
+import 'dart:typed_data';
 import 'package:flutter/widgets.dart';
-
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart' as c;
+import 'package:blurhash_dart/blurhash_dart.dart';
 // Files
 import './profile_page.dart';
 import '../api/unsplash_api_service.dart';
 import '../models/unsplash_photo.dart';
 import '../utils/date.dart';
+import '../widgets/background_images.dart';
 import '../widgets/month.dart';
 
 enum BottomTabOption {
@@ -91,14 +94,22 @@ class _HomePageState extends State<HomePage> {
   void initState() {
     month = DateTime.now().month;
     year = DateTime.now().year;
-    initImages();
-
     super.initState();
   }
 
-  void initImages() async {
-    images = await UnsplashAPIService.getPhotos(12);
-    print(images);
+  Future<List<UnsplashPhoto>> initImages() async {
+    return await UnsplashAPIService.getPhotos(12);
+    // print(images);
+  }
+
+  Uint8List loadBlurHash(String blurHash) {
+    Uint8List pixels;
+    try {
+      pixels = decodeBlurHash(blurHash, 300, 300);
+    } catch (e) {
+      print("Blur Exception: + ${e.toString()}");
+    }
+    return pixels;
   }
 
   void onHorizontalDragEnd(c.DragEndDetails value) {
@@ -122,38 +133,39 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        color: c.CupertinoDynamicColor.resolve(
-          c.CupertinoColors.systemBackground,
-          context,
+    return Stack(
+      children: [
+        Container(
+          color: c.CupertinoDynamicColor.resolve(
+            c.CupertinoColors.systemBackground,
+            context,
+          ),
         ),
-        image: DecorationImage(
-          fit: BoxFit.cover,
-          image: AssetImage('backup-bg-image.JPG'),
-        ),
-      ),
-      child: c.CupertinoPageScaffold(
-        backgroundColor: Color(0x00000000),
-        navigationBar: c.CupertinoNavigationBar(
-          automaticallyImplyLeading: false,
+        BackgroundImage(month: month),
+        c.CupertinoPageScaffold(
           backgroundColor: Color(0x00000000),
-          trailing: YearButton(),
-          border: null,
-        ),
-        child: SafeArea(
-          child: c.GestureDetector(
-            onHorizontalDragEnd: onHorizontalDragEnd,
+          navigationBar: c.CupertinoNavigationBar(
+            backgroundColor: Color(0x00000000),
+            leading: PreferanceButton(),
+            trailing: YearButton(),
+            border: null,
+          ),
+          child: SafeArea(
             child: Container(
-              padding: c.EdgeInsets.symmetric(horizontal: 12),
-              child: Month(
-                month: month,
-                year: year,
+              child: c.GestureDetector(
+                onHorizontalDragEnd: onHorizontalDragEnd,
+                child: Container(
+                  padding: c.EdgeInsets.symmetric(horizontal: 12),
+                  child: Month(
+                    month: month,
+                    year: year,
+                  ),
+                ),
               ),
             ),
           ),
         ),
-      ),
+      ],
     );
   }
 }
