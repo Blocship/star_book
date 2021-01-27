ed025c68a71e8937bf958ef0bdde7a70c49371b9
khyatisaini <sainikhyati2001@gmail.com>
2021-01-28 00:37:11 +0530

    replaced hardcoded strings in profile page

--- lib/screens/profile_page.dart
+++ lib/screens/profile_page.dart
@@ -6,6 +6,7 @@ import '../controllers/global_setting.dart';
 import '../models/global_setting.dart';
 import '../styles/style.dart';
 import '../widgets/my_container.dart';
+import '../controllers/activity.dart';
 
 /// Profile Page displays user details
 /// Such as
@@ -79,7 +80,7 @@ class Stats extends StatelessWidget {
             crossAxisAlignment: c.CrossAxisAlignment.start,
             children: [
               Text(
-                '30',
+                ActivityController.points().toString(),
                 style: Style.title(context),
               ),
               SizedBox(height: 6),
