f7cdd5f9399c4a961df2f7be2d780462b75846b4
khyatisaini <sainikhyati2001@gmail.com>
2021-01-27 19:38:01 +0530

    switching between themes

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ import './models/activity.dart';
 import './models/global_setting.dart';
 import './models/mood.dart';
 import './routes/route_generator.dart';
+import './controllers/global_setting.dart';
 
 /// Starting point of the application.
 void main() async {
@@ -38,22 +39,43 @@ Future<void> hiveInitialize() async {
   await Hive.openBox(globalSettingBoxName);
 }
 
+// function to return brightness for theme data
+Brightness get brightness {
+  var mode = GlobalSettingController.getBrightnessOption();
+  Brightness themeMode;
+  if (mode == BrightnessOption.light) {
+    themeMode = Brightness.light;
+  }
+  else if (mode == BrightnessOption.dark) {
+    themeMode = Brightness.dark;
+  }
+  else if (mode == BrightnessOption.auto) {
+    themeMode = WidgetsBinding.instance.window.platformBrightness;
+  }
+  return themeMode;
+}
+
 /// MyApp is the most Parent widget and initialises the main route.
 ///
 /// [HomePage] initial route
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return c.CupertinoApp(
-      initialRoute: '/username_add',
-      title: "StarBook",
-      theme: c.CupertinoThemeData(),
-      localizationsDelegates: [
-        DefaultMaterialLocalizations.delegate,
-        c.DefaultCupertinoLocalizations.delegate,
-        DefaultWidgetsLocalizations.delegate,
-      ],
-      onGenerateRoute: (settings) => RouteGenerator.mainRoute(settings),
+    return c.ValueListenableBuilder(
+      valueListenable: Hive.box(globalSettingBoxName).listenable(),
+      builder: (context, box, widget) {
+        return c.CupertinoApp(
+          initialRoute: '/username_add',
+          title: "StarBook",
+          theme: c.CupertinoThemeData(brightness: brightness),
+          localizationsDelegates: [
+            DefaultMaterialLocalizations.delegate,
+            c.DefaultCupertinoLocalizations.delegate,
+            DefaultWidgetsLocalizations.delegate,
+          ],
+          onGenerateRoute: (settings) => RouteGenerator.mainRoute(settings),
+        );
+      }
     );
   }
 }
