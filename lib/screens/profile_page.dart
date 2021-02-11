38d546a6ca5b268db87a889cb5ca11cd239cba01
spiderxm <mrigank.rkee@rediffmail.com>
2021-02-11 15:31:04 +0530

    Adding function to greet user relative to the time.

--- lib/screens/profile_page.dart
+++ lib/screens/profile_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart' as c;
+import 'package:flutter/material.dart' as m;
 import 'package:flutter/widgets.dart';
 import 'package:star_book/widgets/pie_chart.dart';
 
@@ -110,10 +111,23 @@ class PreferanceButton extends StatelessWidget {
 
 /// Greeting widget displays greeting based on time.
 class Greeting extends StatelessWidget {
+  String greeting() {
+    m.TimeOfDay timeOfDay = m.TimeOfDay.now();
+    // greetings List contains various greeting messages.
+    List greetings = ["Good Morning", "Good Night", "Hello"];
+    if (timeOfDay.hour >= 6 && timeOfDay.hour <= 10) {
+      return greetings[0];
+    } else if (timeOfDay.hour >= 20 && timeOfDay.hour <= 24) {
+      return greetings[1];
+    } else {
+      return greetings[2];
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Text(
-      'Greetings',
+      greeting(),
       style: Style.largeTitle(context),
     );
   }
