e085a5114edf75cd23b527bc54d0d9d5591819e8
hashirshoaeb <hashirshoaeb@gmail.com>
2021-03-06 15:33:45 +0500

    initializing mock data removed

--- lib/main.dart
+++ lib/main.dart
@@ -25,7 +25,7 @@ void main() async {
     DeviceOrientation.portraitUp
   ]);
   await hiveInitialize();
-  await ActivityController.initialize();
+  // await ActivityController.initialize();
   await DotEnv().load('.env');
   runApp(MyApp());
 }
