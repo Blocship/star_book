9b1802213e8c436c0047adb7690611feaf483aa5
Affan <affansultan901@gmail.com>
2022-10-28 06:45:54 -0700

    Journal Api created

---
+++ lib/api/journal_api.dart
@@ -0,0 +1,11 @@
+import 'package:star_book/api/base_api.dart';
+import 'package:star_book/models/journal/journal.dart';
+
+abstract class IJournalApi extends BaseApi {
+  static const String collectionName = 'journalCollection';
+
+  Stream<List<Journal>> getJournalStream();
+
+  Future<void> delete(String journalId);
+  Future post(Journal journal);
+}
