ed6efeedaac787d89bb7b4f4f33f27b10be3c6c3
SyedMuhammadOsama <s.m.osama33@gmail.com>
2022-10-31 15:38:09 +0500

    Journal Api updated

--- lib/api/journal_api.dart
+++ lib/api/journal_api.dart
@@ -4,11 +4,35 @@ import 'package:star_book/packages/hive_collection.dart';
 
 abstract class IJournalApi extends BaseApi {
   static const String collectionName = 'journalCollection';
-
-  Stream<List<Journal>> getJournalStream();
-
+  Future<List<Journal>> getAll();
+  Future<Journal> getById(String journalId);
   Future<void> delete(String journalId);
   Future post(Journal journal);
 }
 
+class LSJournalApi extends IJournalApi {
+  final HiveCollectionReference<Journal> collection;
+  LSJournalApi({required this.collection});
 
+  @override
+  Future<Journal> getById(String journalId) async {
+    return collection.doc(journalId)!.get();
+  }
+
+  @override
+  Future<List<Journal>> getAll() async {
+    return collection.docs().map((e) => e.get()).toList();
+  }
+
+  @override
+  Future<void> delete(String journalId) {
+    // TODO: implement delete
+    throw UnimplementedError();
+  }
+
+  @override
+  Future post(Journal journal) {
+    // TODO: implement post
+    throw UnimplementedError();
+  }
+}
