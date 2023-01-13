93e725835156053286fcd595ea7c1888146d8e2c
hashirshoaeb <hashirshoaeb@gmail.com>
2023-01-13 19:26:08 +0500

    refer: #290 base theme under construction

---
+++ lib/theme/base_theme.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+class ForegroundTheme {
+  final Color primaryColor;
+  final Color secondaryColor;
+  final Color tertiaryColor;
+  final Color quaternaryColor;
+  final Color quinary;
+
+  const ForegroundTheme({
+    required this.primaryColor,
+    required this.secondaryColor,
+    required this.tertiaryColor,
+    required this.quaternaryColor,
+    required this.quinary,
+    // senary, septenary, octonary, nonary, denary
+  });
+}
+
+class BackgroundTheme {
+  final Color primaryColor;
+
+  const BackgroundTheme({
+    required this.primaryColor,
+  });
+}
+
+class FontTheme {
+  final TextStyle heading1;
+  final TextStyle heading2;
+  final TextStyle heading3;
+  final TextStyle body1;
+  final TextStyle body2;
+  final TextStyle caption;
+
+  const FontTheme({
+    required this.heading1,
+    required this.heading2,
+    required this.heading3,
+    required this.body1,
+    required this.body2,
+    required this.caption,
+  });
+}
+
+class FontFamily {
+  final String primary;
+
+  FontFamily({
+    required this.primary,
+  });
+}
+
+abstract class BaseTheme {
+  ForegroundTheme get foregroundTheme;
+  BackgroundTheme get backgroundTheme;
+  FontTheme get fontTheme;
+  FontFamily get fontFamily;
+
+  ThemeData get theme {
+    return ThemeData(
+      primaryColor: foregroundTheme.primaryColor,
+      appBarTheme: AppBarTheme(
+        color: foregroundTheme.primaryColor,
+      ),
+    );
+  }
+}
