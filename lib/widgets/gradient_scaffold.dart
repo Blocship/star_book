9528d46f64ce7f97b19e49e9a8322d2eb7df5154
hashirshoaeb <hashirshoaeb@gmail.com>
2023-02-01 17:25:47 +0500

    minor updates

--- lib/widgets/gradient_scaffold.dart
+++ lib/widgets/gradient_scaffold.dart
@@ -2,11 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:star_book/theme/styling/gradient_scaffold_style.dart';
 
 class GradientScaffold extends StatelessWidget {
-  final Widget body;
+  final Widget? body;
 
   const GradientScaffold({
     Key? key,
-    required this.body,
+    this.body,
   }) : super(key: key);
 
   @override
@@ -18,63 +18,83 @@ class GradientScaffold extends StatelessWidget {
           Scaffold(
             body: body,
           ),
-          const Positioned(
+          Positioned(
             top: 80,
             right: -10,
-            child: OrangeShadow(
+            child: BlurShadow(
               shadowWidth: 180,
               shadowHeight: 240,
               borderRadius: 200,
               spreadRadius: 100,
               blurRadius: 80,
               shadowOpacity: 0.17,
+              color: Theme.of(context)
+                  .extension<GradientScaffoldStyle>()!
+                  .secondaryColor!
+                  .withOpacity(0.17),
             ),
           ),
-          const Positioned(
+          Positioned(
             top: 200,
             right: 30,
-            child: OrangeShadow(
+            child: BlurShadow(
               shadowWidth: 40,
               shadowHeight: 40,
               borderRadius: 120,
               spreadRadius: 120,
               blurRadius: 80,
               shadowOpacity: 0.1,
+              color: Theme.of(context)
+                  .extension<GradientScaffoldStyle>()!
+                  .secondaryColor!
+                  .withOpacity(0.1),
             ),
           ),
-          const Positioned(
+          Positioned(
             bottom: 10,
-            child: BlueShadow(
+            child: BlurShadow(
               shadowWidth: 360,
               shadowHeight: 300,
               borderRadius: 120,
               spreadRadius: 120,
               blurRadius: 80,
               shadowOpacity: 0.17,
+              color: Theme.of(context)
+                  .extension<GradientScaffoldStyle>()!
+                  .primaryColor!
+                  .withOpacity(0.17),
             ),
           ),
-          const Positioned(
+          Positioned(
             bottom: 220,
             left: 140,
-            child: BlueShadow(
+            child: BlurShadow(
               shadowWidth: 40,
               shadowHeight: 40,
               borderRadius: 120,
               spreadRadius: 40,
               blurRadius: 100,
               shadowOpacity: 0.1,
+              color: Theme.of(context)
+                  .extension<GradientScaffoldStyle>()!
+                  .primaryColor!
+                  .withOpacity(0.1),
             ),
           ),
-          const Positioned(
+          Positioned(
             top: 380,
             left: 160,
-            child: PinkShadow(
+            child: BlurShadow(
               shadowWidth: 20,
               shadowHeight: 20,
               borderRadius: 120,
               spreadRadius: 30,
               blurRadius: 35,
               shadowOpacity: 0.08,
+              color: Theme.of(context)
+                  .extension<GradientScaffoldStyle>()!
+                  .tertiaryColor!
+                  .withOpacity(0.08),
             ),
           ),
         ],
@@ -83,15 +103,16 @@ class GradientScaffold extends StatelessWidget {
   }
 }
 
-class OrangeShadow extends StatelessWidget {
+class BlurShadow extends StatelessWidget {
   final double shadowWidth;
   final double shadowHeight;
   final double borderRadius;
   final double spreadRadius;
   final double blurRadius;
   final double shadowOpacity;
+  final Color color;
 
-  const OrangeShadow({
+  const BlurShadow({
     Key? key,
     required this.shadowWidth,
     required this.shadowHeight,
@@ -99,6 +120,7 @@ class OrangeShadow extends StatelessWidget {
     required this.spreadRadius,
     required this.blurRadius,
     required this.shadowOpacity,
+    required this.color,
   }) : super(key: key);
 
   @override
@@ -112,90 +134,7 @@ class OrangeShadow extends StatelessWidget {
           BoxShadow(
             spreadRadius: spreadRadius,
             blurRadius: blurRadius,
-            color: Theme.of(context)
-                .extension<GradientScaffoldStyle>()!
-                .secondaryColor!
-                .withOpacity(shadowOpacity),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class BlueShadow extends StatelessWidget {
-  final double shadowWidth;
-  final double shadowHeight;
-  final double borderRadius;
-  final double spreadRadius;
-  final double blurRadius;
-  final double shadowOpacity;
-
-  const BlueShadow({
-    Key? key,
-    required this.shadowWidth,
-    required this.shadowHeight,
-    required this.borderRadius,
-    required this.spreadRadius,
-    required this.blurRadius,
-    required this.shadowOpacity,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: shadowWidth,
-      height: shadowHeight,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(borderRadius),
-        boxShadow: [
-          BoxShadow(
-            spreadRadius: spreadRadius,
-            blurRadius: blurRadius,
-            color: Theme.of(context)
-                .extension<GradientScaffoldStyle>()!
-                .primaryColor!
-                .withOpacity(shadowOpacity),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class PinkShadow extends StatelessWidget {
-  final double shadowWidth;
-  final double shadowHeight;
-  final double borderRadius;
-  final double spreadRadius;
-  final double blurRadius;
-  final double shadowOpacity;
-
-  const PinkShadow({
-    Key? key,
-    required this.shadowWidth,
-    required this.shadowHeight,
-    required this.borderRadius,
-    required this.spreadRadius,
-    required this.blurRadius,
-    required this.shadowOpacity,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: shadowWidth,
-      height: shadowHeight,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(borderRadius),
-        boxShadow: [
-          BoxShadow(
-            spreadRadius: spreadRadius,
-            blurRadius: blurRadius,
-            color: Theme.of(context)
-                .extension<GradientScaffoldStyle>()!
-                .tertiaryColor!
-                .withOpacity(shadowOpacity),
+            color: color,
           ),
         ],
       ),
