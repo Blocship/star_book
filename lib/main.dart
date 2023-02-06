47582db6a553512bd74b57cea91516e07270ece5
Abdullah Ahmed Soomro <abdullahsoomro245@gmail.com>
2023-02-06 10:37:52 +0500

    UI TextFormField
    - Minor Updates

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:star_book/presentation/injector/injector.dart';
 import 'package:star_book/presentation/screen/calendar/custom_calendar.dart';
-import 'package:star_book/presentation/shared/text_field.dart';
 import 'package:star_book/theme/ultramarine_light.dart';
 
 void main() async {
@@ -17,28 +16,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: UltramarineLightTheme().theme,
-      home: Scaffold(
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: const [
-            SizedBox(
-              height: 80,
-              child: CustomTextFormField(
-                heading: 'Title',
-                label: 'Enter  YOur Mood',
-              ),
-            ),
-            SizedBox(
-              // height: 80,
-              child: CustomTextFormField(
-                heading: 'Title',
-                label: 'Enter  YOur Mood',
-              ),
-            ),
-          ],
-        ),
-      ),
+      home: const CustomCalendar(),
     );
   }
 }
--- lib/presentation/shared/text_field.dart
+++ lib/presentation/shared/text_field.dart
@@ -1,5 +1,3 @@
-import 'dart:developer';
-
 import 'package:flutter/material.dart';
 import 'package:star_book/theme/styling/theme_color_style.dart';
 
@@ -37,138 +35,6 @@ class PrimaryTextField extends StatelessWidget {
   }
 }
 
-/// - Make GenericTextField
-/// - Active and Inactive Concept
-/// - Use Animation
-// class JournalTextField extends StatelessWidget {
-//   final String hintText;
-//   final String header;
-//   // final _fieldKey = GlobalKey();
-//   const JournalTextField({
-//     Key? key,
-//     required this.hintText,
-//     required this.header,
-//   }) : super(key: key);
-//   @override
-//   Widget build(BuildContext context) {
-//     // final textHeight = _fieldKey.currentContext!.findRenderObject() as  RenderBox;
-//     return Container(
-//       width: 306,
-//       // height: textHeight.size.height,
-//       height: 80,
-//       padding: const EdgeInsets.symmetric(horizontal: 12.0),
-//       decoration: BoxDecoration(
-//         borderRadius: BorderRadius.circular(8.0),
-//         border: Border.all(color: Colors.grey),
-//       ),
-//       child: Column(
-//         crossAxisAlignment: CrossAxisAlignment.start,
-//         children: [
-//           const SizedBox(height: 14),
-//           Text(header),
-//           const SizedBox(height: 14),
-//           SizedBox(
-//             width: 306,
-//             child: TextFormField(
-//               // key: _fieldKey,
-//               minLines: 1,
-//               maxLines: null,
-//               keyboardType: TextInputType.multiline,
-//               decoration: InputDecoration(
-//                 isDense: true,
-//                 hintText: hintText,
-//                 contentPadding: const EdgeInsets.all(0),
-//                 border: const OutlineInputBorder(borderSide: BorderSide.none),
-//               ),
-//             ),
-//           ),
-//         ],
-//       ),
-//     );
-//   }
-// }
-// class CustomTextFormField extends StatefulWidget {
-//   final String heading;
-//   final String label;
-//
-//   const CustomTextFormField({
-//     Key? key,
-//     required this.heading,
-//     required this.label,
-//   }) : super(key: key);
-//
-//   @override
-//   State<CustomTextFormField> createState() => _CustomTextFormFieldState();
-// }
-
-// class _CustomTextFormFieldState extends State<CustomTextFormField> {
-//   final FocusNode focusNode = FocusNode();
-//   final GlobalKey _formFieldKey = GlobalKey();
-//   final TextEditingController controller = TextEditingController();
-//   @override
-//   void initState() {
-//     focusNode.addListener(() {
-//       setState(() {});
-//     });
-//     super.initState();
-//   }
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     // log(focusNode.hasFocus.toString());
-//     return Container(
-//       width: 306,
-//       // height: 200,
-//       // height: _formFieldKey.currentContext?.size?.height ?? 60,
-//       // height: textHeight.size.height,
-//       padding: const EdgeInsets.symmetric(horizontal: 12.0),
-//       decoration: BoxDecoration(
-//         // color: Colors.blue,
-//         borderRadius: BorderRadius.circular(8.0),
-//         border: Border.all(color: Colors.grey),
-//       ),
-//       child: Column(
-//         crossAxisAlignment: CrossAxisAlignment.start,
-//         children: [
-//           AnimatedDefaultTextStyle(
-//             style: focusNode.hasFocus
-//                 ? const TextStyle(
-//                     fontSize: 18,
-//                     color: Colors.red,
-//                   )
-//                 : const TextStyle(
-//                     fontSize: 17,
-//                     color: Colors.blue,
-//                   ),
-//             duration: const Duration(milliseconds: 200),
-//             child: Text(widget.heading),
-//           ),
-//           const SizedBox(height: 8),
-//           SizedBox(
-//             height: 30,
-//             child: TextFormField(
-//               // key: _formFieldKey,
-//               expands: true,
-//               minLines: null,
-//               maxLines: null,
-//               focusNode: focusNode,
-//               controller: controller,
-//               keyboardType: TextInputType.multiline,
-//               decoration: InputDecoration(
-//                 contentPadding: EdgeInsets.zero,
-//                 isDense: true,
-//                 hintText: widget.label,
-//                 border: const OutlineInputBorder(
-//                   borderSide: BorderSide.none,
-//                 ),
-//               ),
-//             ),
-//           ),
-//         ],
-//       ),
-//     );
-//   }
-// }
 class CustomTextFormField extends StatefulWidget {
   final String heading;
   final String label;
@@ -187,6 +53,7 @@ class _CustomTextFormFieldState extends State<CustomTextFormField> {
   final FocusNode focusNode = FocusNode();
 
   final TextEditingController controller = TextEditingController();
+
   @override
   void initState() {
     focusNode.addListener(() {
@@ -207,25 +74,29 @@ class _CustomTextFormFieldState extends State<CustomTextFormField> {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
+          const SizedBox(height: 8),
           AnimatedDefaultTextStyle(
             style: focusNode.hasFocus
-                ? const TextStyle(
-                    fontSize: 18,
-                    color: Colors.red,
-                  )
-                : const TextStyle(
-                    fontSize: 17,
-                    color: Colors.blue,
-                  ),
+                ? Theme.of(context).textTheme.bodySmall!.copyWith(
+                      fontWeight: FontWeight.w400,
+                    )
+                : Theme.of(context).textTheme.bodyLarge!.copyWith(
+                      fontWeight: FontWeight.w400,
+                      color: Theme.of(context)
+                          .extension<ThemeColorStyle>()!
+                          .secondaryColor,
+                    ),
             duration: const Duration(milliseconds: 200),
             child: Text(widget.heading),
           ),
           const SizedBox(height: 8),
           LayoutBuilder(
             builder: (context, constraints) {
-              return Container(
-                width: constraints.maxWidth,
-                height: constraints.maxHeight,
+              return SizedBox(
+                width: 306,
+                height: 40,
+                // width: constraints.maxWidth,
+                // height: constraints.maxHeight,
                 child: TextFormField(
                   expands: true,
                   minLines: null,
@@ -237,6 +108,9 @@ class _CustomTextFormFieldState extends State<CustomTextFormField> {
                     contentPadding: EdgeInsets.zero,
                     isDense: true,
                     hintText: widget.label,
+                    hintStyle: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                          fontWeight: FontWeight.w400,
+                        ),
                     border: const OutlineInputBorder(
                       borderSide: BorderSide.none,
                     ),
