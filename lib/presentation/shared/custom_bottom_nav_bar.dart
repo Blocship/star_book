720f23575f3b1a549ce21d5d2073a77be43badb9
Abdullah <abdullahsoomro245@gmail.com>
2023-02-10 21:49:21 +0500

    UI Bottom Nav Bar
    - Custom Bottom Navigation Bar Completed.

--- lib/presentation/shared/custom_bottom_nav_bar.dart
+++ lib/presentation/shared/custom_bottom_nav_bar.dart
@@ -1,89 +1,62 @@
 import 'package:flutter/material.dart';
-import 'package:star_book/widgets/gradient_scaffold.dart';
 
-class MainScreen extends StatefulWidget {
-  const MainScreen({Key? key}) : super(key: key);
+class CustomBottomNavBar extends StatefulWidget {
+  final int currentIndex;
+  final List<BottomNavigationBarItem> items;
+  final ValueChanged<int> onTap;
+  final Color? selectedItemColor;
+  final Color? unselectedItemColor;
+  final TextStyle? selectedLabelStyle;
+  final TextStyle? unselectedLabelStyle;
+  final Color? backgroundColor;
+  const CustomBottomNavBar({
+    Key? key,
+    required this.currentIndex,
+    required this.items,
+    required this.onTap,
+    this.selectedItemColor,
+    this.unselectedItemColor,
+    this.selectedLabelStyle,
+    this.unselectedLabelStyle,
+    this.backgroundColor,
+  }) : super(key: key);
 
   @override
-  State<MainScreen> createState() => _MainScreenState();
+  State<CustomBottomNavBar> createState() => _CustomBottomNavBarState();
 }
 
-class _MainScreenState extends State<MainScreen> {
-  late int _selectedPage;
-  late final PageController pageController;
-
-  @override
-  void initState() {
-    super.initState();
-    _selectedPage = 0;
-    pageController = PageController(initialPage: _selectedPage);
-  }
-
-  void onPageChanged(int page) {
-    setState(() {
-      _selectedPage = page;
-    });
-  }
-
-  void onNavBarItemTapped(int page) {
-    pageController.jumpToPage(page);
-  }
-
-  List<Widget> pages = <Widget>[];
-
+class _CustomBottomNavBarState extends State<CustomBottomNavBar> {
   @override
   Widget build(BuildContext context) {
-    return GradientScaffold(
-      body: PageView(
-        controller: pageController,
-        onPageChanged: onPageChanged,
-        children: pages,
-      ),
-      bottomNavigationBar: Container(
-        padding: const EdgeInsets.fromLTRB(30, 0, 30, 20),
-        child: ClipRRect(
-          borderRadius: BorderRadius.circular(100),
-          child: Theme(
-            data: ThemeData(
-                splashColor: Colors.transparent,
-                highlightColor: Colors.transparent),
-            child: BottomNavigationBar(
-              selectedItemColor: const Color(0xFF7B7CFF),
-              unselectedItemColor: const Color(0xFF7B7CFF).withOpacity(0.4),
-              selectedLabelStyle: Theme.of(context)
-                  .textTheme
-                  .bodyMedium!
-                  .copyWith(
-                      fontWeight: FontWeight.w500,
-                      color: const Color(0xFF7B7CFF)),
-              unselectedLabelStyle: Theme.of(context)
-                  .textTheme
-                  .bodyMedium!
-                  .copyWith(
-                      fontWeight: FontWeight.w400,
-                      color: const Color(0xFF7B7CFF).withOpacity(0.4)),
-              elevation: 0,
-              currentIndex: _selectedPage,
-              onTap: onNavBarItemTapped,
-              iconSize: 25,
-              items: const [
-                BottomNavigationBarItem(
-                  icon: ImageIcon(
-                    AssetImage('assets/icons/home_outlined.png'),
-                  ),
-                  label: 'Home',
+    return Stack(
+      alignment: Alignment.center,
+      children: [
+        Positioned(
+          bottom: 20,
+          child: SizedBox(
+            width: MediaQuery.of(context).size.width * 0.8,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(100),
+              child: Theme(
+                data: ThemeData(
+                  splashColor: Colors.transparent,
+                  highlightColor: Colors.transparent,
                 ),
-                BottomNavigationBarItem(
-                  icon: ImageIcon(
-                    AssetImage('assets/icons/account_circle.png'),
-                  ),
-                  label: 'Profile',
+                child: BottomNavigationBar(
+                  backgroundColor: widget.backgroundColor,
+                  selectedItemColor: widget.selectedItemColor,
+                  unselectedItemColor: widget.unselectedItemColor,
+                  selectedLabelStyle: widget.selectedLabelStyle,
+                  unselectedLabelStyle: widget.unselectedLabelStyle,
+                  currentIndex: widget.currentIndex,
+                  onTap: widget.onTap,
+                  items: widget.items,
                 ),
-              ],
+              ),
             ),
           ),
         ),
-      ),
+      ],
     );
   }
 }
