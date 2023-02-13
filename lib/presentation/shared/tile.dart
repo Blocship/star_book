aa708c4a193fed61cee6a37518b95ecf6c80ea9c
Abdullah Ahmed Soomro <abdullahsoomro245@gmail.com>
2023-02-13 15:52:12 +0500

    UI Setting Tile

--- lib/presentation/shared/tile.dart
+++ lib/presentation/shared/tile.dart
@@ -14,29 +14,26 @@ class CustomTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      width: MediaQuery.of(context).size.width * 0.95,
-      child: ListTile(
-        title: Text(
-          title,
-          style: Theme.of(context).textTheme.bodyLarge!.copyWith(
-                fontWeight: FontWeight.w600,
-                color: const Color(0xFF1F1F1F),
-              ),
-        ),
-        subtitle: Text(
-          subtitle,
-          style: Theme.of(context)
-              .textTheme
-              .bodyMedium!
-              .copyWith(fontWeight: FontWeight.w400),
-        ),
-        trailing: const Icon(
-          Icons.keyboard_arrow_right,
-          color: Color(0xFF1F1F1F),
-        ),
-        onTap: onTap,
+    return ListTile(
+      title: Text(
+        title,
+        style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+              fontWeight: FontWeight.w600,
+              color: const Color(0xFF1F1F1F),
+            ),
       ),
+      subtitle: Text(
+        subtitle,
+        style: Theme.of(context)
+            .textTheme
+            .bodyMedium!
+            .copyWith(fontWeight: FontWeight.w400),
+      ),
+      trailing: const Icon(
+        Icons.keyboard_arrow_right,
+        color: Color(0xFF1F1F1F),
+      ),
+      onTap: onTap,
     );
   }
 }
