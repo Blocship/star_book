e1ed025a4466ac33d7f169bd1bdb596b15f11e63
Abdullah Ahmed Soomro <abdullahsoomro245@gmail.com>
2023-02-23 14:38:37 +0500

    Feature Journal Create Cubit
    - Journal Create Cubit.

---
+++ lib/cubits/journal_create_cubit.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_form_builder/flutter_form_builder.dart';
+import 'package:star_book/cubits/cubit_state/cubit_state.dart';
+import 'package:star_book/domain/models/journal/journal.dart';
+import 'package:star_book/domain/repository/journal_repo.dart';
+import 'package:star_book/presentation/shared/form_models/jounral_form_model.dart';
+
+class JournalCreateCubit extends Cubit<CubitState<Journal>> {
+  final GlobalKey<FormBuilderState> formKey;
+  final JournalRepo journalRepo;
+
+  JournalCreateCubit({required this.journalRepo, required this.formKey})
+      : super(const InitialState());
+
+  Future<void> postJournal({String? id, DateTime? createdAt}) async {
+    formKey.currentState?.save();
+    if (formKey.currentState?.validate() ?? false) {
+      final formData = JournalFormModel.fromJson(formKey.currentState!.value);
+      emit(const LoadingState());
+      await journalRepo.addJournal(
+        Journal(
+          id: 'id',
+          createdAt: DateTime.now(),
+          updatedAt: DateTime.now(),
+          mood: formData.mood,
+          title: formData.title,
+          memo: formData.memo,
+        ),
+      );
+    }
+  }
+}
