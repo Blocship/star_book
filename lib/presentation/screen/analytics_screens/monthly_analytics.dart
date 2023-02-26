4277db652b69a1fd8264d8192de054663119d89b
Abdullah <abdullahsoomro245@gmail.com>
2023-02-26 21:02:53 +0500

    Feature Analytics Screen Cubit Implementation.
    - Added.

--- lib/presentation/screen/analytics_screens/monthly_analytics.dart
+++ lib/presentation/screen/analytics_screens/monthly_analytics.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:star_book/cubits/analytiics_screen_cubit.dart';
+import 'package:star_book/cubits/cubit_state/cubit_state.dart';
+import 'package:star_book/domain/models/mood/mood.dart';
+import 'package:star_book/domain/repository/mood_repo.dart';
+import 'package:star_book/presentation/injector/injector.dart';
 import 'package:star_book/presentation/shared/legends_chart.dart';
 import 'package:star_book/presentation/theme/styling/doughnut_chart_style.dart';
 import 'package:star_book/presentation/utils/extension.dart';
@@ -21,17 +27,29 @@ class MonthlyAnalyticsTab extends StatelessWidget {
       ChartData(x: 'Happy', y: 1.5, color: doughnutChartStyle.quinaryColor),
       ChartData(x: 'Sick', y: 2.0, color: doughnutChartStyle.quaternaryColor),
     ];
-    return Padding(
-      padding:
-          const EdgeInsets.symmetric(horizontal: CustomPadding.mediumPadding),
-      child: Column(
-        children: [
-          MoodDoughnutChart(moodDataMap: data),
-          SizedBox(height: deviceHeight * 0.05),
-          const SelectableTab(),
-          SizedBox(height: deviceHeight * 0.03),
-          const LegendsChart(),
-        ],
+    return BlocProvider(
+      create: (context) => AnalyticsScreenCubit(
+        moodRepo: Injector.resolve<MoodRepo>(),
+      ),
+      child: BlocBuilder<AnalyticsScreenCubit, CubitState<Mood>>(
+        builder: (context, state) {
+          // final getMood = context.read<AnalyticsScreenCubit>().getMood();
+          // final getMoodFreqByMonth = context.read<AnalyticsScreenCubit>().getMoodFrequencyByMonth(month: month, year: year);
+          // final getMoodFreqByRange = context.read<AnalyticsScreenCubit>().getMoodFrequencyByRange(start: start, end: end);
+          return Padding(
+            padding: const EdgeInsets.symmetric(
+                horizontal: CustomPadding.mediumPadding),
+            child: Column(
+              children: [
+                MoodDoughnutChart(moodDataMap: data),
+                SizedBox(height: deviceHeight * 0.05),
+                const SelectableTab(),
+                SizedBox(height: deviceHeight * 0.03),
+                const LegendsChart(),
+              ],
+            ),
+          );
+        },
       ),
     );
   }
@@ -55,38 +73,43 @@ class _SelectableTabState extends State<SelectableTab> {
     final ThemeColorStyle themeColorStyle = context.themeColorStyle;
     final double deviceHeight = context.deviceHeight;
     final double deviceWidth = context.deviceWidth;
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: months.map((month) {
-        return GestureDetector(
-          onTap: () {
-            setState(() {
-              selectedMonths = month;
-            });
-          },
-          child: Container(
-            width: deviceWidth * 0.105,
-            height: deviceHeight * 0.03,
-            decoration: BoxDecoration(
-              color: (month == selectedMonths)
-                  ? themeColorStyle.secondaryColor
-                  : themeColorStyle.secondaryColor.withOpacity(0.03),
-              borderRadius: BorderRadius.circular(100),
-            ),
-            alignment: Alignment.center,
-            child: Text(
-              month,
-              style: textTheme.labelLarge!.copyWith(
-                fontWeight: FontWeight.w400,
-                color: (month == selectedMonths)
-                    ? themeColorStyle.quinaryColor
-                    : themeColorStyle.secondaryColor,
+    return BlocBuilder<AnalyticsScreenCubit, CubitState<Mood>>(
+      builder: (context, state) {
+        // final getMoodFreqByMonth = context.read<AnalyticsScreenCubit>().getMoodFrequencyByMonth(month: month, year: year);
+        return Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: months.map((month) {
+            return GestureDetector(
+              onTap: () {
+                setState(() {
+                  selectedMonths = month;
+                });
+              },
+              child: Container(
+                width: deviceWidth * 0.105,
+                height: deviceHeight * 0.03,
+                decoration: BoxDecoration(
+                  color: (month == selectedMonths)
+                      ? themeColorStyle.secondaryColor
+                      : themeColorStyle.secondaryColor.withOpacity(0.03),
+                  borderRadius: BorderRadius.circular(100),
+                ),
+                alignment: Alignment.center,
+                child: Text(
+                  month,
+                  style: textTheme.labelLarge!.copyWith(
+                    fontWeight: FontWeight.w400,
+                    color: (month == selectedMonths)
+                        ? themeColorStyle.quinaryColor
+                        : themeColorStyle.secondaryColor,
+                  ),
+                ),
               ),
-            ),
-          ),
+            );
+          }).toList(),
         );
-      }).toList(),
+      },
     );
   }
 }
