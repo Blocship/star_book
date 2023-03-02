352ab7eb8be92b48db193d459420f183cae9de04
Abdullah Ahmed Soomro <abdullahsoomro245@gmail.com>
2023-03-02 19:32:25 +0500

    BUG Doughnut Chart
    - Minor Updates.

--- lib/presentation/widgets/doughnut_chart.dart
+++ lib/presentation/widgets/doughnut_chart.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:star_book/presentation/theme/styling/theme_color_style.dart';
 import 'package:star_book/presentation/utils/extension.dart';
@@ -18,17 +20,15 @@ class MoodDoughnutChart extends StatefulWidget {
 class _MoodDoughnutChartState extends State<MoodDoughnutChart> {
   @override
   Widget build(BuildContext context) {
+    double highestValue = highestSector(widget.moodDataMap);
     final TextTheme textTheme = context.textTheme;
     final ThemeColorStyle themeColorStyle = context.themeColorStyle;
     final double deviceWidth = context.deviceWidth;
-
+    print(highestValue);
     return SfCircularChart(
       annotations: <CircularChartAnnotation>[
         CircularChartAnnotation(
-          widget: PhysicalModel(
-            shape: BoxShape.circle,
-            color: themeColorStyle.quinaryColor,
-          ),
+          widget: ArrowIndicator(direction: pi / 2),
         ),
         CircularChartAnnotation(
           widget: CircleAvatar(
@@ -74,6 +74,18 @@ class _MoodDoughnutChartState extends State<MoodDoughnutChart> {
       ],
     );
   }
+
+  double highestSector(List<ChartData> data) {
+    double highestAngle = 0.0;
+    for (int i = 0; i < data.length; i++) {
+      // Checking for largest value in the list
+      if (data[i].y > highestAngle) {
+        highestAngle = data[i].y;
+      }
+    }
+
+    return highestAngle;
+  }
 }
 
 class ChartData {
@@ -82,4 +94,49 @@ class ChartData {
   final String x;
   final double y;
   final Color color;
+}
+
+class ArrowIndicator extends StatelessWidget {
+  final double direction;
+  const ArrowIndicator({Key? key, required this.direction}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Transform.rotate(
+      // angle should be in radian
+      // so rad = degree * pi / 180
+      // since motion is relative, we move the phone
+      // not the arrow, so -1 for inverting direction
+      angle: (direction),
+      alignment: Alignment.center,
+      child: Center(
+        child: CustomPaint(
+          size: const Size(50, 50),
+          painter: DrawTriangleShape(),
+        ),
+      ),
+    );
+  }
+}
+
+class DrawTriangleShape extends CustomPainter {
+  var painter = Paint()
+    ..color = Colors.white
+    ..style = PaintingStyle.fill;
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    var path = Path();
+    path.moveTo(size.width / 2, size.height * -1.8); // For Top point
+    path.lineTo(0, size.height * -1); // For Bottom right point
+    path.lineTo(size.width, size.height * -1); // For Bottom left point
+    // path.lineTo(size.height, size.width); // For Bottom left point
+    path.close();
+    canvas.drawPath(path, painter);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return false;
+  }
 }
