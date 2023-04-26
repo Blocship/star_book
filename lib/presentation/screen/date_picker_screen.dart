7178de714bf0f1dcf7d15033324db79e185470d1
Abdullah Ahmed Soomro <abdullahsoomro245@gmail.com>
2023-04-26 19:03:06 +0500

    Refactor Bottom Sheet UI
    - Refactored

--- lib/presentation/screen/date_picker_screen.dart
+++ lib/presentation/screen/date_picker_screen.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_form_builder/flutter_form_builder.dart';
 import 'package:intl/intl.dart';
 import 'package:star_book/presentation/routes/routes.dart';
-import 'package:star_book/presentation/shared/app_bar.dart';
 import 'package:star_book/presentation/shared/text_field.dart';
 import 'package:star_book/presentation/theme/styling/theme_color_style.dart';
 import 'package:star_book/presentation/utils/extension.dart';
@@ -61,6 +60,12 @@ class _CustomDatePickerFormFieldState
       effectiveFocusNode.unfocus();
       showModalBottomSheet(
         context: context,
+        isScrollControlled: true,
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.vertical(
+            top: Radius.circular(40),
+          ),
+        ),
         builder: (context) {
           return DatePickerBottomSheet(
             initialDate: dateTime ?? DateTime.now(),
@@ -115,55 +120,98 @@ class _DatePickerBottomSheetState extends State<DatePickerBottomSheet> {
     final TextTheme textTheme = context.textTheme;
     final ThemeColorStyle themeColorStyle = context.themeColorStyle;
     final double deviceHeight = context.deviceHeight;
+    final double deviceWidth = context.deviceWidth;
 
-    return Scaffold(
-      appBar: const PrimaryAppBar(
-        showLeading: false,
-        centerTitle: 'Select Date',
-      ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(
-          horizontal: CustomPadding.mediumPadding,
+    return Container(
+      height: deviceHeight * 0.61,
+      padding:
+          const EdgeInsets.symmetric(horizontal: CustomPadding.mediumPadding),
+      decoration: const BoxDecoration(
+        // color: Colors.blueGrey,
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(40),
+          topRight: Radius.circular(40),
         ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            const SizedBox(height: 30),
-            Container(
-              height: deviceHeight * 0.06,
-              decoration: BoxDecoration(
-                color: themeColorStyle.septenaryColor,
-                borderRadius: BorderRadius.circular(8.0),
-              ),
-              alignment: Alignment.center,
-              child: Text(
-                'MM--DD--YY',
-                style: textTheme.bodyLarge!.copyWith(
-                  fontWeight: FontWeight.w400,
-                  color: themeColorStyle.secondaryColor,
+      ),
+      child: Stack(
+        children: [
+          Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const SizedBox(height: 30),
+              Container(
+                width: deviceWidth * 0.2,
+                height: 4,
+                decoration: BoxDecoration(
+                  color: Colors.black.withOpacity(0.3),
+                  borderRadius: BorderRadius.circular(50),
                 ),
               ),
-            ),
-            const SizedBox(height: 30),
-            DatePicker(
-              initialDate: widget.initialDate,
-              onDateChanged: (DateTime date) {
-                setState(() {
-                  dateTime = date;
-                });
-                // widget.onDateChanged?.call(date);
+              const SizedBox(height: 30),
+              SizedBox(
+                height: 25,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Text(
+                      'Select Date',
+                      style: textTheme.bodyLarge!.copyWith(
+                        fontWeight: FontWeight.w700,
+                        color: themeColorStyle.secondaryColor,
+                      ),
+                    ),
+                    SizedBox(width: deviceWidth * 0.27),
+                    GestureDetector(
+                      onTap: () {},
+                      child: Icon(
+                        Icons.close,
+                        color: themeColorStyle.secondaryColor,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 30),
+              Container(
+                height: deviceHeight * 0.06,
+                decoration: BoxDecoration(
+                  color: themeColorStyle.septenaryColor,
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+                alignment: Alignment.center,
+                child: Text(
+                  'MM--DD--YY',
+                  style: textTheme.bodyLarge!.copyWith(
+                    fontWeight: FontWeight.w400,
+                    color: themeColorStyle.secondaryColor,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 30),
+              DatePicker(
+                initialDate: widget.initialDate,
+                onDateChanged: (DateTime date) {
+                  setState(() {
+                    dateTime = date;
+                  });
+                  // widget.onDateChanged?.call(date);
+                },
+              ),
+            ],
+          ),
+          Positioned(
+            right: 0,
+            bottom: 18,
+            child: SecondaryFloatingActionButton(
+              onTap: () {
+                widget.onDone?.call(dateTime);
+                context.shouldPop();
               },
+              child: const Icon(Icons.check),
             ),
-          ],
-        ),
-      ),
-      floatingActionButton: SecondaryFloatingActionButton(
-        onTap: () {
-          widget.onDone?.call(dateTime);
-          context.shouldPop();
-        },
-        child: const Icon(Icons.check),
+          )
+        ],
       ),
     );
   }
