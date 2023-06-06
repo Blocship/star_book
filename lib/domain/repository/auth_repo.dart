d5e16ee12741f0012913dcc5cb261d85916f58e3
hashirshoaeb <hashirshoaeb@gmail.com>
2023-06-06 07:48:23 +0500

    updates

--- lib/domain/repository/auth_repo.dart
+++ lib/domain/repository/auth_repo.dart
@@ -1,9 +1,7 @@
+import 'package:star_book/domain/models/user/user.dart';
 import 'package:star_book/domain/repository/base_repo.dart';
 
 abstract class AuthRepo extends BaseRepo {
-  Future<void> signIn({required String email, required String password});
-  Future<void> signUp({required String email, required String password});
-  Future<void> signOut();
   Future<bool> isSignedIn();
-  Future<String> getUser();
+  Future<User> getUser();
 }
