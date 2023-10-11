899cbc3ff8d916d22dd7967794fba84f0b0951c3
Abdullah <abdullahsoomro245@gmail.com>
2023-10-11 20:43:01 +0500

    Feature Firebase Crashlytics
    - Integrate Firebase.
    - Add Firebase Crashlytics.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
+import 'dart:async';
 import 'dart:io';
 
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:path_provider/path_provider.dart';
@@ -19,34 +22,40 @@ String createDirectory({required String path}) {
 }
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  final config = Config();
-  assert(
-    config.isDevelopment,
-    'Please run in dev environment for debugging. i.e. --dart-define=flavor=dev',
-  );
-  final directory = await getApplicationDocumentsDirectory();
-  await LocalDatabase.initialise(
-    directory: createDirectory(
-      path: directory.path + config.cacheDirectory,
-    ),
-  );
-  await Injector.initialise();
-  final isFreshInstall = Injector.resolve<AppSettings>().isFreshInstall;
-  if (isFreshInstall) {
-    Injector.resolve<MoodRepo>().addDefaultMoods();
-  }
+  runZonedGuarded<Future<void>>(() async {
+    WidgetsFlutterBinding.ensureInitialized();
+    await Firebase.initializeApp();
+    FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
+    final config = Config();
+    assert(
+      config.isDevelopment,
+      'Please run in dev environment for debugging. i.e. --dart-define=flavor=dev',
+    );
+    final directory = await getApplicationDocumentsDirectory();
+    await LocalDatabase.initialise(
+      directory: createDirectory(
+        path: directory.path + config.cacheDirectory,
+      ),
+    );
+    await Injector.initialise();
+    final isFreshInstall = Injector.resolve<AppSettings>().isFreshInstall;
+    if (isFreshInstall) {
+      Injector.resolve<MoodRepo>().addDefaultMoods();
+    }
 
-  /// Run this statement when you reinstall your app
-  /// for adding moods in backend (run only once)
-  // Injector.resolve<MoodRepo>().addDefaultMoods();
-  SystemChrome.setSystemUIOverlayStyle(
-    const SystemUiOverlayStyle(
-      statusBarColor: Colors.transparent,
-      statusBarIconBrightness: Brightness.dark,
-    ),
-  );
-  runApp(const MyApp());
+    /// Run this statement when you reinstall your app
+    /// for adding moods in backend (run only once)
+    // Injector.resolve<MoodRepo>().addDefaultMoods();
+    SystemChrome.setSystemUIOverlayStyle(
+      const SystemUiOverlayStyle(
+        statusBarColor: Colors.transparent,
+        statusBarIconBrightness: Brightness.dark,
+      ),
+    );
+    runApp(const MyApp());
+  },
+      (error, stack) =>
+          FirebaseCrashlytics.instance.recordError(error, stack, fatal: true));
 }
 
 class MyApp extends StatelessWidget {
